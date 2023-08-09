c0a57511073caef04af704dcd62ae45b1ae82dc6
vincent <vincent@kali.kali>
2023-08-09 21:56:14 +0300

    removed gitignore

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.use(express.json())
 app.use(helmet())
 app.use(morgan("common"))
 app.use(cors({
-    origin: 'https://deskclient.netlify.app'
+    origin: 'deskclient.netlify.app'
 }));
 app.use(cookieParser())
 
