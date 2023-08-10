f50c7b3f382c421aedda91bda71e468c88f55228
vincent <vincent@kali.kali>
2023-08-10 08:58:49 +0300

    upgraded cors

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ mongoose.connection.on("disconnected",() => {
 
 //middlewares
 app.use(express.json())
-app.use(cors({origin: ["https://desk-client.vercel.app", "http://localhost:3000"],credentials: true}));
+app.use(cors({origin: ["https://desk-client.vercel.app", "deskclient.netlify.app"],credentials: true}));
 app.use(helmet())
 app.use(morgan("common"))
 app.use(cookieParser())
